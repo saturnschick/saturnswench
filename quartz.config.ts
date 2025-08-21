bba1eb10660841863569b158f91030ebbd95f2e9
saturnschick <shiela.redmitablet@gmail.com>
2025-08-21 20:07:21 +0200

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -81,7 +81,7 @@ const config: QuartzConfig = {
       Plugin.FolderPage(),
       Plugin.TagPage(),
       Plugin.ContentIndex({
-        enableSiteMap: true,
+        enableSiteMap: false,
         enableRSS: true,
       }),
       Plugin.Assets(),
